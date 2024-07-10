8c043b1abc15a18872f429a0a77d6a91fb635e61
KarlaMartinez1 <00021523@uca.edu.sv>
2024-07-09 22:59:16 -0600

    🎨:adding

--- src/main/java/com/example/parcialfinalpoo/Clases/Cliente.java
+++ src/main/java/com/example/parcialfinalpoo/Clases/Cliente.java
@@ -14,29 +14,29 @@ public class Cliente { //0021523 creacion clase compra
     }
 
     public int getId() { //0021523 obtener id
-        return id;
+        return id;  //0021523 retorno id
     }
     public void setId(int id) { //0021523 setear id
-        this.id = id;
+        this.id = id;  //0021523 setea id como id
     }
     public String getNombre() { //0021523 obtener nombre
-        return nombre;
+        return nombre; //0021523 retorno nombre
     }
     public void setNombre(String nombre) { //0021523 setear nombre
-        this.nombre = nombre;
+        this.nombre = nombre; //0021523 setea nombre como nombre
     }
 
     public int getCantidadCompras() { //0021523 obtener cantidad
-        return cantidadCompras;
+        return cantidadCompras; //0021523 retorna cantidad
     }
     public void setCantidadCompras(int cantidadCompras) { //0021523 setear cantidad
-        this.cantidadCompras = cantidadCompras;
+        this.cantidadCompras = cantidadCompras; //0021523 setea cantidad como cantidad
     }
     public double getTotalCompras() { //0021523 obtener total gastado
-        return totalCompras;
+        return totalCompras; //0021523 retorna gasto en compras
     }
     public void setTotalCompras(double totalCompras) { //0021523 setear total gastado
-        this.totalCompras = totalCompras;
+        this.totalCompras = totalCompras; //0021523 setea total de compras como total de compras
     }
 
 }
