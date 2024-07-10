86ef1812710b89ab20e08535e76bf0ab26d1106b
KarlaMartinez1 <00021523@uca.edu.sv>
2024-07-09 22:41:00 -0600

    🎨:Add comments clase Cliente

--- src/main/java/com/example/parcialfinalpoo/Clases/Cliente.java
+++ src/main/java/com/example/parcialfinalpoo/Clases/Cliente.java
@@ -1,41 +1,41 @@
 package com.example.parcialfinalpoo.Clases;
 
-public class Cliente {
-    private int id;
-    private String nombre;
-    private int cantidadCompras;
-    private double totalCompras;
+public class Cliente { //0021523 creacion clase compra
+    private int id; //0021523 id cliente
+    private String nombre; //0021523 nombre cliente
+    private int cantidadCompras; //0021523 cantidad de compras realizadas por el cliente
+    private double totalCompras; //0021523 total gastado en las compras
 
-    public Cliente(int id, String nombre, int CantidadCompras, double TotalCompras) {
-        this.id = id;
-        this.nombre = nombre;
-        this.cantidadCompras = CantidadCompras;
-        this.totalCompras = TotalCompras;
+    public Cliente(int id, String nombre, int CantidadCompras, double TotalCompras) { //0021523 constructor
+        this.id = id; //0021523 id a constructor
+        this.nombre = nombre; //0021523 nombre a constructor
+        this.cantidadCompras = CantidadCompras; //0021523 cantidad a constructor
+        this.totalCompras = TotalCompras; //0021523 total a constructor
     }
 
-    public int getId() {
+    public int getId() { //0021523 obtener id
         return id;
     }
-    public void setId(int id) {
+    public void setId(int id) { //0021523 setear id
         this.id = id;
     }
-    public String getNombre() {
+    public String getNombre() { //0021523 obtener nombre
         return nombre;
     }
-    public void setNombre(String nombre) {
+    public void setNombre(String nombre) { //0021523 setear nombre
         this.nombre = nombre;
     }
 
-    public int getCantidadCompras() {
+    public int getCantidadCompras() { //0021523 obtener cantidad
         return cantidadCompras;
     }
-    public void setCantidadCompras(int cantidadCompras) {
+    public void setCantidadCompras(int cantidadCompras) { //0021523 setear cantidad
         this.cantidadCompras = cantidadCompras;
     }
-    public double getTotalCompras() {
+    public double getTotalCompras() { //0021523 obtener total gastado
         return totalCompras;
     }
-    public void setTotalCompras(double totalCompras) {
+    public void setTotalCompras(double totalCompras) { //0021523 setear total gastado
         this.totalCompras = totalCompras;
     }
 
